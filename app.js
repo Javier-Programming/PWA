3530c9a7c97add49029062137389a7049fd391d5
Javier-Programming <javierdiazmsound@gmail.com>
2025-05-08 15:12:48 -0500

    Versión inicial de la batería PWA

---
+++ app.js
@@ -0,0 +1,7 @@
+function playSound(name) {
+  const audio = new Audio(`sounds/${name}.wav`);
+  audio.play();
+}
+
+document.getElementById('snare').addEventListener('click', () => playSound('snare'));
+document.getElementById('kick').addEventListener('click', () => playSound('kick'));
---
+++ service-worker.js
@@ -0,0 +1,26 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('bateria-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/app.js',
+        '/manifest.json',
+        '/service-worker.js',
+        '/sounds/snare.wav',
+        '/sounds/kick.wav',
+        '/icon-192.png',
+        '/icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
