763be2353c3fba9686388e381651d9e655998e54
Javier-Programming <javierdiazmsound@gmail.com>
2025-05-08 15:46:04 -0500

    Actualización

--- app.js
+++ app.js
@@ -3,5 +3,16 @@ function playSound(name) {
   audio.play();
 }
 
-document.getElementById('snare').addEventListener('click', () => playSound('snare'));
-document.getElementById('kick').addEventListener('click', () => playSound('kick'));
+document
+  .getElementById("snare")
+  .addEventListener("click", () => playSound("snare"));
+document
+  .getElementById("kick")
+  .addEventListener("click", () => playSound("kick"));
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("./service-worker.js")
+    .then(() => console.log("Service Worker registrado"))
+    .catch(console.error);
+}
