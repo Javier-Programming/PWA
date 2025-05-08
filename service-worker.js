c4176742501fea9e5e08d736775e40c449c5ec78
Javier-Programming <javierdiazmsound@gmail.com>
2025-05-08 15:49:25 -0500

    Actualización

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,25 @@
-self.addEventListener('install', function(event) {
+self.addEventListener("install", function (event) {
   event.waitUntil(
-    caches.open('bateria-cache').then(function(cache) {
+    caches.open("bateria-cache").then(function (cache) {
       return cache.addAll([
-        '/',
-        'index.html',
-        'style.css',
-        'app.js',
-        'manifest.json',
-        'service-worker.js',
-        'sounds/snare.wav',
-        'sounds/kick.wav',
-        'icon-192.png',
-        'icon-512.png'
+        "/",
+        "index.html",
+        "style.css",
+        "app.js",
+        "manifest.json",
+        "service-worker.js",
+        "./sounds/snare.wav",
+        "./sounds/kick.wav",
+        "icon-192.png",
+        "icon-512.png",
       ]);
     })
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener("fetch", function (event) {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
+    caches.match(event.request).then(function (response) {
       return response || fetch(event.request);
     })
   );
